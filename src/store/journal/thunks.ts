a889b64e57dda841377e7be1c4bc9dd0ceda502a
Miguel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2024-12-27 00:49:42 -0500

    add empty note

--- src/store/journal/thunks.ts
+++ src/store/journal/thunks.ts
@@ -1,8 +1,21 @@
-import { Dispatch } from "@reduxjs/toolkit"
+import { AppDispatch } from "../store";
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { RootState } from "@reduxjs/toolkit/query"
+import { FiresbasDB } from "../../firebase";
 
 export const startNewNote = () => {
-  return async ( dispatch: Dispatch, getState: () ) => {
+  return async ( dispatch: AppDispatch, getState: () => RootState ) => {
 
-    
+    const { uid } = getState().auth.user;
+
+    const newNote = {
+      title: '',
+      body: '',
+      date: new Date().getTime(),
+    }
+
+    const newDoc = doc( collection( FiresbasDB, `${ uid }/journal/notes` )  );
+    const setDocResp = await setDoc( newDoc, newNote );
+    console.log({ newDoc, setDocResp });
   }
 }
